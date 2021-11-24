6ae17527e6d310415b6ceb1cbd2adf6478d7b798
Dewald928 <dewald123@rocketmail.com>
2021-11-24 13:20:22 +0200

    Date change on click

--- src/components/TransactionTable.vue
+++ src/components/TransactionTable.vue
@@ -7,14 +7,29 @@
       :loading="loading"
       @request="onRequest"
       :rows-per-page-options="[10, 20, 50]"
-    )
+    ) 
+      q-tr( slot="header" slot-scope="props", :props="props" )
+        q-th(
+          v-for="col in props.cols"
+          :key="col.name"
+          :props="props"
+          @click="col.name==='date' ? showAge=!showAge : null"
+        )
+          template(
+            v-if="col.name==='date'"
+            class="date-column"
+          )
+            q-tooltip() Click to change format
+          | {{ col.label }}            
+
+
       q-tr( slot="body" slot-scope="props" :props="props" )
         q-td( key="hash" )
           transaction-field( :transaction-hash="props.row.hash" )
         q-td( key="block" )
           block-field( :block="props.row.block" )
         q-td( key="date" )
-          date-field( :epoch="props.row.epoch" )
+          date-field( :epoch="props.row.epoch", :showAge="showAge")
         q-td( key="method" ) {{ props.row.method ? props.row.method : '' }}
         q-td( key="from" )
           address-field( :address="props.row.from" )
@@ -31,40 +46,45 @@ import TransactionField from "components/TransactionField";
 
 const columns = [
   {
-    name: 'hash',
-    label: 'TX Hash',
-    align: 'left'
-  },{
-    name: 'block',
-    label: 'Block',
-    align: 'left'
-  },{
-    name: 'date',
-    label: 'Date',
-    align: 'left'
-  },{
-    name: 'method',
-    label: 'Method',
-    align: 'left'
-  },{
-    name: 'from',
-    label: 'From',
-    align: 'left'
-  },{
-    name: 'to',
-    label: 'To / Interacted with',
-    align: 'left'
-  },{
-    name: 'value',
-    label: 'Value',
-    align: 'left'
+    name: "hash",
+    label: "TX Hash",
+    align: "left"
   },
-]
-
+  {
+    name: "block",
+    label: "Block",
+    align: "left"
+  },
+  {
+    name: "date",
+    label: "Date",
+    align: "left"
+  },
+  {
+    name: "method",
+    label: "Method",
+    align: "left"
+  },
+  {
+    name: "from",
+    label: "From",
+    align: "left"
+  },
+  {
+    name: "to",
+    label: "To / Interacted with",
+    align: "left"
+  },
+  {
+    name: "value",
+    label: "Value",
+    align: "left"
+  }
+];
 
 export default {
   name: "TransactionTable",
-  components: {TransactionField, DateField, BlockField, AddressField },
+  components: { TransactionField, DateField, BlockField, AddressField },
   props: {
     title: {
       type: String,
@@ -87,49 +107,57 @@ export default {
       total: null,
       loading: false,
       pagination: {
-        sortBy: 'date',
+        sortBy: "date",
         descending: true,
         page: 1,
         rowsPerPage: 10,
         rowsNumber: 0
-      }
-    }
+      },
+      showAge: true
+    };
   },
   mounted() {
     this.onRequest({
       pagination: this.pagination
-    })
+    });
   },
   methods: {
     async onRequest(props) {
       this.loading = true;
 
-      const { page, rowsPerPage, sortBy, descending } = props.pagination
+      const { page, rowsPerPage, sortBy, descending } = props.pagination;
 
-      let result = await this.$evmEndpoint.get(this.getPath(props))
+      let result = await this.$evmEndpoint.get(this.getPath(props));
       if (this.total == null)
         this.pagination.rowsNumber = result.data.total.value;
 
-      this.pagination.page = page
-      this.pagination.rowsPerPage = rowsPerPage
-      this.pagination.sortBy = sortBy
-      this.pagination.descending = descending
+      this.pagination.page = page;
+      this.pagination.rowsPerPage = rowsPerPage;
+      this.pagination.sortBy = sortBy;
+      this.pagination.descending = descending;
 
-      this.transactions.splice(0, this.transactions.length, ...result.data.transactions);
+      this.transactions.splice(
+        0,
+        this.transactions.length,
+        ...result.data.transactions
+      );
       for (const transaction of this.transactions) {
         try {
-          if (transaction.input_data === '0x')
-            continue;
+          if (transaction.input_data === "0x") continue;
 
-          const contract = await this.$contractManager.getContract(transaction.to);
-          if (!contract)
-            continue;
+          const contract = await this.$contractManager.getContract(
+            transaction.to
+          );
+          if (!contract) continue;
 
-          const parsedTransaction = await contract.parseTransaction(transaction.input_data);
-          if (parsedTransaction)
-            transaction.method = parsedTransaction.name;
+          const parsedTransaction = await contract.parseTransaction(
+            transaction.input_data
+          );
+          if (parsedTransaction) transaction.method = parsedTransaction.name;
         } catch (e) {
-          console.error(`Failed to set method for transaction, error was: ${e.message}`);
+          console.error(
+            `Failed to set method for transaction, error was: ${e.message}`
+          );
         }
       }
       this.setRows(page, rowsPerPage);
@@ -137,29 +165,27 @@ export default {
     },
     setRows(page, rowsPerPage) {
       // TODO: do this differently?
-        this.rows = this.transactions;
+      this.rows = this.transactions;
     },
     getPath(props) {
-      const { page, rowsPerPage, descending } = props.pagination
-      let path = `/v2/evm/get_transactions?limit=${rowsPerPage === 0 ? 500 : rowsPerPage}`
+      const { page, rowsPerPage, descending } = props.pagination;
+      let path = `/v2/evm/get_transactions?limit=${
+        rowsPerPage === 0 ? 500 : rowsPerPage
+      }`;
       const filter = Object.assign({}, this.filter ? this.filter : {});
-      if (filter.address)
-        path += `&address=${filter.address}`
+      if (filter.address) path += `&address=${filter.address}`;
 
-      if (filter.block)
-        path += `&block=${filter.block}`
+      if (filter.block) path += `&block=${filter.block}`;
 
-      if (filter.hash)
-        path += `&hash=${filter.hash}`
+      if (filter.hash) path += `&hash=${filter.hash}`;
 
       path += `&skip=${(page - 1) * rowsPerPage}`;
-      path += `&sort=${descending ? 'desc' : 'asc'}`;
+      path += `&sort=${descending ? "desc" : "asc"}`;
 
       return path;
     }
   }
-
-}
+};
 </script>
 
 <style scoped>
