b67de322449c52c8d1be682af591aaddf70a082a
JPBeukes <jpbeukes27@gmail.com>
2021-12-08 15:15:44 +0200

    Removed HomeInfo component name and reformat file

--- src/components/HomeInfo.vue
+++ src/components/HomeInfo.vue
@@ -1,73 +1,67 @@
 <template>
-
-        <div class="row homeInfo">
-
-          <div class="col-xs-6 col-md-3 q-pa-md">
-            <div class="row items-center">
-
-              <div class="col-12 items-center">
-                  <div class="column items-center text-subtitle2">TLOS Price</div>
-              </div>
-
-              <div class="col-12">
-                <div class="column items-center text-h6 text-weight-bold">$1.15</div>
-              </div>
-
-            </div>
-          </div>
-
-          <div class="col-xs-6 col-md-3 q-pa-md">
-            <div class="row items-center">
-
-              <div class="col-12 items-center">
-                  <div class="column items-center text-subtitle2">TLOS Market Cap</div>
-              </div>
-
-              <div class="col-12">
-                <div class="column items-center text-h6 text-weight-bold">$270,171,568.58</div>
-              </div>
-
-            </div>
-          </div>
-
-         <div class="col-xs-6 col-md-3 q-pa-md">
-            <div class="row items-center">
-
-              <div class="col-12 items-center">
-                  <div class="column items-center text-subtitle2">Transaction Count</div>
-              </div>
-
-              <div class="col-12">
-                <div class="column items-center text-h6 text-weight-bold">45,487,249,587</div>
-              </div>
-
-            </div>
-          </div>
-
-          <div class="col-xs-6 col-md-3 q-pa-md">
-            <div class="row items-center">
-
-              <div class="col-12 items-center">
-                  <div class="column items-center text-subtitle2">Blocks</div>
-              </div>
-
-              <div class="col-12">
-                <div class="column items-center text-h6 text-weight-bold">186,427,544</div>
-              </div>
-
-            </div>
-          </div>
-
+  <div class="row homeInfo">
+    <div class="col-xs-6 col-md-3 q-pa-md">
+      <div class="row items-center">
+        <div class="col-12 items-center">
+          <div class="column items-center text-subtitle2">TLOS Price</div>
         </div>
 
+        <div class="col-12">
+          <div class="column items-center text-h6 text-weight-bold">$1.15</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-xs-6 col-md-3 q-pa-md">
+      <div class="row items-center">
+        <div class="col-12 items-center">
+          <div class="column items-center text-subtitle2">TLOS Market Cap</div>
+        </div>
+
+        <div class="col-12">
+          <div class="column items-center text-h6 text-weight-bold">
+            $270,171,568.58
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-xs-6 col-md-3 q-pa-md">
+      <div class="row items-center">
+        <div class="col-12 items-center">
+          <div class="column items-center text-subtitle2">
+            Transaction Count
+          </div>
+        </div>
+
+        <div class="col-12">
+          <div class="column items-center text-h6 text-weight-bold">
+            45,487,249,587
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-xs-6 col-md-3 q-pa-md">
+      <div class="row items-center">
+        <div class="col-12 items-center">
+          <div class="column items-center text-subtitle2">Blocks</div>
+        </div>
+
+        <div class="col-12">
+          <div class="column items-center text-h6 text-weight-bold">
+            186,427,544
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  components: { },
-  name: "Home Info"
+  components: {}
 };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
