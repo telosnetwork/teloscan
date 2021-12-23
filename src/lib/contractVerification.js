8072667adcca616bb90dbe7e48d46c87949c4b6c
DonaldPeat <don@caleos.io>
2021-12-23 15:02:25 -0600

    feat: base skeleton for compiling
    chore: update solc version to 8.3 (from 8.11)  use new syntax

---
+++ src/lib/contractVerification.js
@@ -0,0 +1,34 @@
+// const path = require('path');
+// const fs = require('fs');
+const solc = require('solc');
+
+var input = {
+    language: 'Solidity',
+    sources: {
+      'test.sol': {
+        content: `pragma solidity ^0.8.11;
+            contract C { 
+                function set(uint _value) public { value = _value; }
+                function get() public view returns (uint) { return value; }
+                uint value;
+            }`
+      }
+    },
+    settings: {
+      outputSelection: {
+        '*': {
+          '*': ['*']
+        }
+      }
+    }
+  };
+  
+  var output = JSON.parse(solc.compile(JSON.stringify(input)));
+  
+  for (var contractName in output.contracts['test.sol']) {
+    const abi = output.contracts['test.sol'][contractName].abi;
+    const bytecode = output.contracts['test.sol'][contractName].evm.bytecode.object;
+    console.log("contract: ",contractName);
+    console.log("bytecode: ", bytecode);
+    console.dir("abi: ", abi);
+  }
