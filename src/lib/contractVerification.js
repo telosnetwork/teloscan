8944bbab850971c0282c8ec1ba27075380891aea
DonaldPeat <don@caleos.io>
2021-12-27 09:43:03 -0600

    feat: const args appended to bytecode if constructor present

--- src/lib/contractVerification.js
+++ src/lib/contractVerification.js
@@ -6,20 +6,21 @@ const Web3EthAbi = require('web3-eth-abi');
 
 const NONE = 'n/a';
 const fileName = 'test.sol';
-const fileContent = `pragma solidity ^0.8.11;
+const fileContent = 
+`pragma solidity ^0.8.11;
 contract A { 
-    function set(uint _value) public {}
-    function get() public view returns (uint) { return value; }
     uint value;
+    function get() public view returns (uint) { return value; }
+    function set(uint _value) public { value = _value; }
 }
 contract B { 
-    function set(uint _value) public { myValue= _value; }
-    function get() public view returns (uint) { return value + myValue; }
+    uint value;
+    uint myValue;
     constructor(uint _constructorArg, address _test) public {
         value = _constructorArg;
     }
-    uint value;
-    uint myValue;
+    function set(uint _value) public { myValue= _value; }
+    function get() public view returns (uint) { return value + myValue; }
 }`;
 const constructorArgs = [42,"0x46ef48e06ff160f311d17151e118c504d015ec6e"]
   
@@ -28,16 +29,16 @@ processFile(fileName, fileContent);
 function processFile(fileName, fileContent){
     const output = compileFile(fileName, fileContent);
     for (let contractName in output.contracts['test.sol']) {
-        const encodedConstructorArgs = NONE; 
-        const decodedConstructorArgs = NONE;
+        let encodedConstructorArgs = NONE; 
+        let decodedConstructorArgs = NONE;
         const bytecode = output.contracts['test.sol'][contractName].evm.bytecode.object;
         const abi = output.contracts['test.sol'][contractName].abi;
         const argTypes = getArgTypes(abi);
 
         if (argTypes.length) {
             try{
-            encodedConstructorArgs = Web3EthAbi.encodeParameters(argTypes, constructorArgs)
-            decodedConstructorArgs = Web3EthAbi.decodeParameters(typesArr, encodedHex) 
+                encodedConstructorArgs = Web3EthAbi.encodeParameters(argTypes, constructorArgs)
+                decodedConstructorArgs = Web3EthAbi.decodeParameters(argTypes, encodedConstructorArgs) 
             }catch(e){
                 console.error(e);
             }
@@ -45,6 +46,9 @@ function processFile(fileName, fileContent){
     
         console.log("contract: ",contractName);
         console.log("bytecode: ", bytecode);
+        if (encodedConstructorArgs !== NONE){
+            console.log("bytecode w/constructor args:")
+        }
         console.log("abi: ", util.inspect(abi, false, null, true));
         console.log("encoded args: ", encodedConstructorArgs);
         console.log("decoded args: ",decodedConstructorArgs);
