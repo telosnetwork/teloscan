ab38916998491bae37157af9e0d3a72d520692f5
DonaldPeat <don@caleos.io>
2021-12-31 22:06:06 -0600

    feat: upload contract files on verify form

--- src/pages/ContractVerification.vue
+++ src/pages/ContractVerification.vue
@@ -38,23 +38,28 @@
         keep-alive
       )
         q-tab-panel(name="options")
-          q-btn-dropdown(color="primary" label="Compiler Version")
+          q-btn-dropdown(color="primary" label="Select Compiler Version" )
               q-list
-                  q-item(v-for="option in compilerOptions" :key='option' clickable v-close-popup @click="setCompiler(option)")
+                  q-item(v-for="option in compilerOptions" :key='option' clickable v-close-popup @click='setCompiler(option)')
                       q-item-section()
                           q-item-label() {{option}}
+          .compiler-version(v-show='compilerSelected') Selected: {{ compilerVersion }}
           q-uploader(
-            url="http://localhost:4444/upload"
+            :factory='uploadFiles'
+            :disabled='!compilerSelected'
             label="upload solidity files"
+            no-thumbnails=true
+            :form-fields='getFormData'
             multiple
             batch
             style="max-width: 300px"
-            accept=".sol"
+            accept='.sol'
             @rejected="onRejected"
           )
 </template>
 
 <script>
+import axios from 'axios';
 import { getCompilerOptions } from 'src/lib/contractVerification';
 
 export default {
@@ -63,15 +68,27 @@ export default {
     return {
       tab: "general",
       compilerOptions: [],
-      compilerVersion: ''
+      compilerVersion: '',
+      
     };
   },
   async mounted() {
       this.compilerOptions = await getCompilerOptions();
   },
+  computed: {
+    compilerSelected(){
+      return this.compilerVersion.length;
+    }
+  },
   methods: {
     setCompiler(option){
       this.compilerVersion = option;
+    },
+    getFormData(){
+      debugger;
+      return [
+        { name: 'compilerVersion', value: this.compilerVersion}
+      ]
     },
     onRejected(e){
       const errorMessage = JSON.stringify(e);
@@ -80,6 +97,25 @@ export default {
           position: 'top',
           message: `Error: ${errorMessage}`
       });
+    },
+    async uploadFiles(fileArray){
+      const fd = new FormData();
+      for (let file of fileArray){
+        fd.append(file.name, file)
+      }
+      fd.append('compilerVersion', this.compilerVersion);
+      try{
+        await axios.post( 'http://localhost:9999/v1/contracts/upload',
+          fd,
+          {
+            onUploadProgress: (progressEvent) => {
+              //@TODO update progress method: updateProgress(Math.round((progressEvent.loaded / progressEvent.total) * 100) / 100)
+            }
+          });
+      }catch(e){
+        console.log(e);
+      }
+
     }
   }
 }
