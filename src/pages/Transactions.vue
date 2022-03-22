7fff88ccf8bbca1aefad47565c249286d1b1f2a3
Don Peat <don@caleos.io>
2022-03-21 22:21:41 -0500

    refactor: remove unused imported components

--- src/pages/Transactions.vue
+++ src/pages/Transactions.vue
@@ -1,25 +1,5 @@
-<template>
-  <q-page class="column justify-center items-center">
-
-    <div class="row q-pa-lg">
-      <div class="text-h4">
-        Explore Transactions
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-12 q-px-sm">
-        <transaction-table title="Recent transactions"></transaction-table>
-      </div>
-    </div>
-
-  </q-page>
-</template>
-
 <script>
-import Search from 'components/Search'
 import TransactionTable from "components/TransactionTable";
-import HomeInfo from "components/HomeInfo.vue";
 export default {
   components: { 
     TransactionTable
@@ -27,3 +7,12 @@ export default {
   name: "Index"
 };
 </script>
+
+<template lang='pug'>
+  q-page.column.justify-center.items-center
+    .row.q-pa-lg
+      .text-h4 Explore Transactions
+    .row
+      .col-12.q-px-sm
+        TransactionTable( title="Recent transactions")
+</template>
