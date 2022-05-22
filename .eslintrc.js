5cc43081e18345ac0b65b5b32dc154846b328c10
Ezra Sowden-Guzman <ezratic@gmail.com>
2022-05-22 18:46:39 +0000

    fix deps errs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,33 @@
 module.exports = {
-    extends: [
-    // add more generic rulesets here, such as:
-        'eslint:recommended',
-        'plugin:vue/recommended', // Use this if you are using Vue.js 2.x.
-    ],
-    rules: {
-        'comma-dangle': ['error', 'always-multiline'],
-        'indent': ['error', 4],
-        'quotes': ['error', 'single'],
+    root: true,
+    parserOptions: {
+        parser: 'babel-eslint',
+        ecmaVersion: 2018,
+        sourceType: 'module',
     },
     env: {
         'browser': true,
         'amd': true,
         'node': true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/recommended',
+    ],
+    plugins: [
+        'vue',
+    ],
+    globals: {
+        ga: true, // Google Analytics
+        __statics: true,
+        process: true,
+    },
+    rules: {
+        // allow debugger during development only
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+        'comma-dangle': ['error', 'always-multiline'],
+        'indent': ['error', 4],
+        'quotes': ['error', 'single'],
     },
 }
