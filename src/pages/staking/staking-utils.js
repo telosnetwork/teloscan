b528a499f4d3a2bccdaa081e0137277bfdabd709
Ezra Sowden-Guzman <ezratic@gmail.com>
2022-08-24 18:50:35 -0400

    WIP add to metamask prompt

--- src/pages/staking/staking-utils.js
+++ src/pages/staking/staking-utils.js
@@ -1,3 +1,6 @@
+import stlosLogo from 'assets/stlos-logo.png';
+import { WEI_PRECISION } from 'src/lib/utils';
+
 const DAY_SECONDS = 86400;
 const HOUR_SECONDS = 3600;
 
@@ -12,4 +15,21 @@ export function formatUnstakePeriod(seconds) {
     } else {
         return `${seconds / DAY_SECONDS} days`;
     }
+}
+
+export async function promptAddToMetamask() {
+    return window.ethereum.request({
+        method: 'wallet_watchAsset',
+        params: {
+            type: 'ERC20',
+            options: {
+                address: process.env.STAKED_TLOS_CONTRACT_ADDRESS,
+                symbol: 'STLOS',
+                decimals: WEI_PRECISION,
+                image: stlosLogo, // eztodo this is broken, results in a string 'img/stlos-logo.png'
+            },
+        },
+    }).catch(({ message }) => {
+        console.error(message);
+    });
 }
