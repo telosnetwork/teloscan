13ec0155a170f06947ac33666b25e75c1022572f
Jesse Schulman <jesse@caleos.io>
2022-08-25 13:35:11 -0700

    Adding APY and TVL to stats

--- src/pages/staking/Staking.vue
+++ src/pages/staking/Staking.vue
@@ -151,7 +151,8 @@
 </template>
 
 <script>
-import { BigNumber } from 'ethers';
+import { ethers, BigNumber } from 'ethers';
+import Big from 'big.js';
 import { mapGetters } from 'vuex';
 
 import { promptAddToMetamask } from 'pages/staking/staking-utils';
@@ -187,6 +188,8 @@ export default {
         tlosBalance: null,
         stlosBalance: null,
         stlosValue: null,
+        stlosTvl: null,
+        stlosApy: null,
         totalUnstakedTlosBalance: null,
         unlockedTlosBalance: null,
         unstakePeriodSeconds: null,
@@ -208,6 +211,14 @@ export default {
         },
         stats() {
             return [{
+                label: 'TVL',
+                value: this.formatWeiForStats(this.stlosTvl),
+                unit: 'TLOS',
+            },{
+                label: 'APY',
+                value: this.stlosApy,
+                unit: '%',
+            },{
                 label: 'Balance',
                 value: this.formatWeiForStats(this.tlosBalance),
                 unit: 'TLOS',
@@ -261,9 +272,78 @@ export default {
     },
     async created() {
         await this.fetchContracts();
+        await this.fetchContractInstances();
+        await this.fetchGlobalStats();
     },
     methods: {
         promptAddToMetamask,
+        async fetchGlobalStats() {
+            this.stlosTvl = await this.stlosContractInstance.totalAssets()
+
+            if (this.stlosTvl.eq('0')) {
+                this.stlosApy = '0';
+                return;
+            }
+
+            const rexPoolResponse = await this.$store.$api.getTableRows({
+                code: 'eosio',
+                scope: 'eosio',
+                table: 'rexpool',
+                limit: '1',
+            });
+
+            if (!rexPoolResponse || !rexPoolResponse.rows.length) {
+                console.error('Failed to fetch rexpool');
+                return;
+            }
+
+            const distConfigResponse = await this.$store.$api.getTableRows({
+                code: 'exrsrv.tf',
+                scope: 'exrsrv.tf',
+                table: 'config',
+                limit: '1',
+            });
+
+            if (!distConfigResponse || !distConfigResponse.rows.length) {
+                console.error('Failed to fetch exrsrv.tf config');
+                return;
+            }
+
+            const payoutsResponse = await this.$store.$api.getTableRows({
+                code: 'exrsrv.tf',
+                scope: 'exrsrv.tf',
+                table: 'payouts',
+                limit: '100',
+            });
+
+            if (!payoutsResponse || !payoutsResponse.rows.length) {
+                console.error('Failed to fetch exrsrv.tf payouts');
+                return;
+            }
+
+
+            const rexStats = rexPoolResponse.rows[0];
+            const distConfig = distConfigResponse.rows[0];
+            const payoutRow = payoutsResponse.rows.find((row) => row.to === 'eosio.rex');
+
+            const annualPayout = new Big(payoutRow.amount).times(new Big(60 * 60 * 24 * 365).div(payoutRow.interval));
+            const fixedRatio = new Big(distConfig.ratio).div(100);
+            const rexTotal = new Big(rexStats.total_lendable.split(' ')[0]);
+            const stlosTotal = new Big(ethers.utils.formatEther(this.stlosTvl));
+
+            const balanceRatio = rexTotal.eq(0) ? -1 : stlosTotal.times(fixedRatio).div(rexTotal);
+
+            if (balanceRatio.eq(0)) {
+                this.stlosApy = '0';
+                return;
+            }
+
+            const rexPayout = annualPayout.div(balanceRatio.plus(1));
+            const stlosPayout = annualPayout.minus(rexPayout);
+            this.stlosApy = stlosPayout.div(stlosTotal).times(100).toFixed(2);
+            console.log(`Rex APY: ${rexPayout.div(rexTotal).times(100).toFixed(2)}%`)
+            console.log(`sTLOS APY: ${this.stlosApy}%`)
+        },
         fetchBalances() {
             if (!this.address) {
                 this.tlosBalance = null;
@@ -283,6 +363,7 @@ export default {
                     console.error(`Failed to fetch account: ${message}`);
                     this.tlosBalance = null;
                 });
+
 
             const stlosPromise = this.stlosContractInstance.balanceOf(this.address)
                 .then((balanceBn) => {
@@ -327,6 +408,7 @@ export default {
                 .catch(({ message }) => {
                     console.error(`Failed to fetch escrow deposits: ${message}`);
                 });
+
 
             return Promise.all([
                 tlosPromise,
