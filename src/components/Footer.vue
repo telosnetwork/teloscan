8567bff97eca36b89e97c35716b93e591a13a3d3
Ezra Sowden-Guzman <ezratic@gmail.com>
2022-08-26 20:52:55 -0400

    fix: remove "made by" text from footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,43 +1,27 @@
+<template>
+<div class="c-footer row">
+    <div class="col-12">
+        <router-link to="/">
+            Teloscan
+        </router-link>
+        &copy;&nbsp;{{ year }}
+    </div>
+</div>
+</template>
+
 <script>
 export default {
     name: 'AppFooter',
-    methods: {
-        currentFullYear() {
-            const current = new Date();
-            return current.getFullYear();
-        },
-    },
+    data: () => ({
+        year: (new Date()).getFullYear(),
+    }),
 };
 </script>
 
-<template lang='pug'>
-  .row.q-mb-lg.relative-bottom
-    .col-xs-12.col-md-6
-      .column.items-center
-        span COPYRIGHT © {{ currentFullYear() }}
-          a( href="/" target="_blank" rel="noopener noreferrer")
-            | &nbsp;Teloscan&nbsp;
-          span.d-none.d-sm-inline-block All rights Reserved
-    .col-xs-12.col-md-6
-      .column.items-center
-        span.float-md-right.d-none.d-md-block.float-right
-          | Made with
-          q-icon.q-pl-sm(
-            name="fas fa-heart red"
-            style="font-size:15px;color:red;padding-bottom:4px"
-          )
-          | +
-          q-icon.q-pr-sm(
-            name="fas fa-coffee red"
-            style="font-size:15px;color:brown;padding-bottom:4px"
-          )
-          | by
-          a(
-            href="https://eosauthority.com/vote/producers?network=telos"
-            target="_blank"
-          ) &nbsp;Yknot &
-          a(
-            href="https://eosauthority.com/vote/producers?network=telos"
-            target="_blank"
-          ) &nbsp;Caleos
-</template>
+<style lang="scss">
+.c-footer {
+    margin: 8px auto 24px;
+    padding: 0 16px;
+    max-width: 1200px;
+}
+</style>
