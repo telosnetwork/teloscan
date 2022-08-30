77005a0bbec6d31cdc96d18ccc384d6095a11b43
Don Peat <don@caleos.io>
2022-08-30 17:50:58 -0500

    fix: base route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { route } from 'quasar/wrappers'
-import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 
 /*
@@ -12,9 +12,7 @@ import routes from './routes'
  */
 
 export default route(function (/* { store, ssrContext } */) {
-    const createHistory = process.env.SERVER
-        ? createMemoryHistory
-        : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
+    const createHistory = createWebHistory;
 
     const Router = createRouter({
         scrollBehavior: () => ({ left: 0, top: 0 }),
@@ -23,7 +21,7 @@ export default route(function (/* { store, ssrContext } */) {
         // Leave this as is and make changes in quasar.conf.js instead!
         // quasar.conf.js -> build -> vueRouterMode
         // quasar.conf.js -> build -> publicPath
-        history: createHistory(process.env.VUE_ROUTER_MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE),
+        history: createHistory(process.env.VUE_ROUTER_BASE),
     })
     return Router
 })
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,13 @@
 const routes = [
     {
-        path: '/',
+        path: '',
         component: () => import('layouts/MainLayout.vue'),
         children: [
-            { path: '', name: 'home', component: () => import('pages/Index.vue') },
+            {
+                path: '',
+                name: 'home',
+                component: () => import('pages/Index.vue')
+            },
         ],
     },
     {
