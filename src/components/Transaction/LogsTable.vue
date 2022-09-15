525202ea7a99d254fc6c0dad487b9665ff367c12
tomtomcrypto <titiomm@gmail.com>
2022-09-15 16:56:56 +0000

    moved shapeLog to LogsViewer component

--- src/components/Transaction/LogsTable.vue
+++ src/components/Transaction/LogsTable.vue
@@ -1,13 +1,13 @@
 <template>
 <div class="q-pa-md">
     <div class="row">
-        <div v-if="shapedLogs === null" class="col-12 u-flex--center">
+        <div v-if="logs === null" class="col-12 u-flex--center">
             <q-spinner size="md" />
         </div>
 
         <div v-else class="col-12">
             <logs-table-row
-                v-for="(log, index) in shapedLogs"
+                v-for="(log, index) in logs"
                 :key="`log-row-${index}`"
                 :log="log"
             />
@@ -17,8 +17,6 @@
 </template>
 
 <script>
-import { TRANSFER_FUNCTION_SIGNATURES } from 'src/lib/functionSignatures';
-
 import LogsTableRow from 'components/Transaction/LogsTableRow'
 
 export default {
@@ -35,50 +33,6 @@ export default {
             type: Array,
             required: true,
         },
-    },
-    data: () => ({
-        shapedLogs: null,
-    }),
-    created() {
-        let contracts = {};
-        this.logs.forEach(async (log) => {
-            let shapedLog = log;
-            if(!this.contract || log.address !== this.contract.address){
-                let log_contract;
-                if (Object.prototype.hasOwnProperty.call(contracts, log.address)){
-                    log_contract = contracts[log.address]
-                } else {
-                    const metadata = await this.$contractManager.checkBucket(log.address);
-                    if (metadata) {
-                        log_contract = await this.$contractManager.getVerifiedContract(log.address.toLowerCase(), metadata);
-                    } else {
-                        log_contract = await this.$contractManager.getContract(log.address.toLowerCase());
-                    }
-                    contracts[log.address] = log_contract;
-                }
-                if(log_contract){
-                    let logs = await log_contract.parseLogs([log]);
-                    shapedLog = logs[0];
-                    shapedLog.address = log.address;
-                    shapedLog.function_signature = shapedLog.topic ? shapedLog.topic.substr(0, 10) : shapedLog.topics[0].substr(0, 10);
-                    shapedLog.name = shapedLog.signature;
-                }
-            }
-
-            if (TRANSFER_FUNCTION_SIGNATURES.includes(shapedLog.function_signature)) {
-                shapedLog.isTransfer = true;
-                try {
-                    shapedLog.token = await this.$contractManager.getTokenData(log.address, 'erc20');
-                } catch (e) {
-                    console.error(`Failed to retrieve contract with address ${log.address}: ${e.message}`);
-                }
-            }
-
-            if (!Array.isArray(this.shapedLogs))
-                this.shapedLogs = [];
-
-            this.shapedLogs.push(shapedLog)
-        });
     },
 }
 </script>
--- src/components/Transaction/LogsViewer.vue
+++ src/components/Transaction/LogsViewer.vue
@@ -17,7 +17,7 @@
             Human-readable logs
         </div>
         <div class="col-12">
-            <logs-table v-if="human_readable" :logs="logs" :contract="contract"/>
+            <logs-table v-if="human_readable" :logs="shapedLogs" :contract="contract"/>
             <json-viewer
                 v-else
                 :value="logs"
@@ -32,6 +32,7 @@
 <script>
 import JsonViewer from 'vue-json-viewer'
 import LogsTable from 'components/Transaction/LogsTable'
+import { TRANSFER_FUNCTION_SIGNATURES } from 'src/lib/functionSignatures';
 
 export default {
     name: 'LogsViewer',
@@ -49,11 +50,48 @@ export default {
             required: true,
         },
     },
-    mounted(){
-        console.log(this.logs);
+
+    created() {
+        let contracts = {};
+        this.logs.forEach(async (log) => {
+            let shapedLog = log;
+            if(!this.contract || log.address !== this.contract.address){
+                let log_contract;
+                if (Object.prototype.hasOwnProperty.call(contracts, log.address)){
+                    log_contract = contracts[log.address]
+                } else {
+                    const metadata = await this.$contractManager.checkBucket(log.address);
+                    if (metadata) {
+                        log_contract = await this.$contractManager.getVerifiedContract(log.address.toLowerCase(), metadata);
+                    } else {
+                        log_contract = await this.$contractManager.getContract(log.address.toLowerCase());
+                    }
+                    contracts[log.address] = log_contract;
+                }
+                if(log_contract){
+                    let logs = await log_contract.parseLogs([log]);
+                    shapedLog = logs[0];
+                    shapedLog.address = log.address;
+                    shapedLog.function_signature = shapedLog.topic ? shapedLog.topic.substr(0, 10) : shapedLog.topics[0].substr(0, 10);
+                    shapedLog.name = shapedLog.signature;
+                }
+            }
+
+            if (TRANSFER_FUNCTION_SIGNATURES.includes(shapedLog.function_signature)) {
+                shapedLog.isTransfer = true;
+                try {
+                    shapedLog.token = await this.$contractManager.getTokenData(log.address, 'erc20');
+                } catch (e) {
+                    console.error(`Failed to retrieve contract with address ${log.address}: ${e.message}`);
+                }
+            }
+
+            this.shapedLogs.push(shapedLog)
+        });
     },
     data: () => ({
         human_readable: true,
+        shapedLogs: [],
     }),
 }
 </script>
