cbbfcb442e26f952d9ffd770b255b0e4704b2927
tomtomcrypto <titiomm@gmail.com>
2022-09-15 15:27:23 +0000

    parse log per log address fix

--- src/components/Transaction/LogsTable.vue
+++ src/components/Transaction/LogsTable.vue
@@ -42,13 +42,21 @@ export default {
     created() {
         this.logs.forEach(async (log) => {
             let shapedLog = log;
-            if(log.address !== this.contract.address){
-                let log_contract = await this.$contractManager.getContract(log.address);
-                let logs = await log_contract.parseLogs([log]);
-                shapedLog = logs[0];
-                shapedLog.address = log.address;
-                shapedLog.function_signature = shapedLog.topic.substr(0, 10);
-                shapedLog.name = shapedLog.signature;
+            if(!this.contract || log.address !== this.contract.address){
+                const metadata = await this.$contractManager.checkBucket(log.address);
+                let log_contract;
+                if (metadata) {
+                    log_contract = await this.$contractManager.getVerifiedContract(log.address.toLowerCase(), metadata);
+                } else {
+                    log_contract = await this.$contractManager.getContract(log.address.toLowerCase());
+                }
+                if(log_contract){
+                    let logs = await log_contract.parseLogs([log]);
+                    shapedLog = logs[0];
+                    shapedLog.address = log.address;
+                    shapedLog.function_signature = shapedLog.topic ? shapedLog.topic.substr(0, 10) : shapedLog.topics[0].substr(0, 10);
+                    shapedLog.name = shapedLog.signature;
+                }
             }
 
             if (TRANSFER_FUNCTION_SIGNATURES.includes(shapedLog.function_signature)) {
