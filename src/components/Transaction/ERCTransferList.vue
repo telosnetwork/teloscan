8d2b69d803b33069246818bd16a782b5c6cab00b
tomtomcrypto <titiomm@gmail.com>
2022-09-16 23:08:06 +0000

    more overrides

--- src/components/Transaction/ERCTransferList.vue
+++ src/components/Transaction/ERCTransferList.vue
@@ -37,7 +37,7 @@ div(class="fit row wrap justify-start items-start content-start")
       div(v-if="type==='ERC721' || type==='ERC1155'" class="col-4")
         strong {{ ` Token : ` }}
         a(:href="'/address/' + transfer.token.address" style="margin-left: 3px;") {{ transfer.token.symbol }}
-        span {{ ' #' + transfer.tokenId }}
+        span(class="word-break") {{ ' #' + transfer.tokenId }}
       div(v-else class="col-4")
         strong {{ ` Token : ` }}
         span {{ transfer.value }}
