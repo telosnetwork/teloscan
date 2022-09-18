d85b011a48fa1ad7d6f028218357d84e252f8329
tomtomcrypto <titiomm@gmail.com>
2022-09-18 19:29:01 +0000

    fix

--- src/lib/abi/index.js
+++ src/lib/abi/index.js
@@ -3,4 +3,4 @@ import erc721MetadataAbi from './erc721Metadata';
 import erc1155Abi from './erc1155';
 import supportsInterfaceAbi from './supportsInterface';
 
-module.exports = [erc721Abi, erc1155Abi, erc721MetadataAbi, supportsInterfaceAbi];
+export default [erc721Abi, erc1155Abi, erc721MetadataAbi, supportsInterfaceAbi];
