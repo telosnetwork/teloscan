6819c89137464e63f0a124090991c5d8fe33bde8
Don Peat <don@caleos.io>
2022-10-19 01:40:08 -0500

    wip: init call to apy method, comment out refactor

--- src/pages/staking/staking-utils.js
+++ src/pages/staking/staking-utils.js
@@ -1,5 +1,5 @@
-import Big from 'big.js';
-import { BigNumber, ethers } from 'ethers';
+// import Big from 'big.js';
+// import { BigNumber, ethers } from 'ethers';
 
 import { WEI_PRECISION } from 'src/lib/utils';
 
@@ -36,78 +36,74 @@ export function formatUnstakePeriod(seconds) {
     return `${quantity} ${unit}`;
 }
 
-/**
- * Calculates and returns the current APY (annual percentage yield) for sTLOS
- *
- * @param   {object} $api     - instance of Telos API, given in a component by this.$store.$api
- * @param   {string} tvl      - total volume locked in wei, as a string
- * @returns {Promise<string>} - calculated APY as a unitless number, eg. "33.25"
- */
 export async function fetchStlosApy($api, tvl) {
-    const tvlBn = BigNumber.from(tvl);
-
-    if (tvlBn.eq('0')) {
-        return '0';
-    }
-
-    const rexPoolResponse = await $api.getTableRows({
-        code: 'eosio',
-        scope: 'eosio',
-        table: 'rexpool',
-        limit: '1',
-    });
-
-    if (!rexPoolResponse || !rexPoolResponse.rows.length) {
-        console.error('Failed to fetch rexpool');
-        return;
-    }
-
-    const distConfigResponse = await $api.getTableRows({
-        code: 'exrsrv.tf',
-        scope: 'exrsrv.tf',
-        table: 'config',
-        limit: '1',
-    });
-
-    if (!distConfigResponse || !distConfigResponse.rows.length) {
-        console.error('Failed to fetch exrsrv.tf config');
-        return;
-    }
-
-    const payoutsResponse = await $api.getTableRows({
-        code: 'exrsrv.tf',
-        scope: 'exrsrv.tf',
-        table: 'payouts',
-        limit: '100',
-    });
-
-    if (!payoutsResponse || !payoutsResponse.rows.length) {
-        console.error('Failed to fetch exrsrv.tf payouts');
-        return;
-    }
-
-    const rexStats = rexPoolResponse.rows[0];
-    const distConfig = distConfigResponse.rows[0];
-    const payoutRow = payoutsResponse.rows.find((row) => row.to === 'eosio.rex');
-
-    const annualPayout = new Big(payoutRow.amount).times(new Big(60 * 60 * 24 * 365).div(payoutRow.interval));
-    const fixedRatio = new Big(distConfig.ratio).div(100);
-    const rexTotal = new Big(rexStats.total_lendable.split(' ')[0]);
-    const stlosTotal = new Big(ethers.utils.formatEther(tvlBn));
-
-    const balanceRatio = rexTotal.eq(0) ? -1 : stlosTotal.times(fixedRatio).div(rexTotal.add(stlosTotal));
-
-    if (balanceRatio.eq(0)) {
-        return '0';
-    }
-
-    const rexPayout = annualPayout.div(balanceRatio.plus(1));
-    const stlosPayout = annualPayout.minus(rexPayout);
-
-    const apy = stlosPayout.div(stlosTotal).times(100).toFixed(2);
-
-    return apy;
+    await $api.fetchStlosApy(tvl);
 }
+
+//     const tvlBn = BigNumber.from(tvl);
+
+//     if (tvlBn.eq('0')) {
+//         return '0';
+//     }
+
+//     const rexPoolResponse = await $api.getTableRows({
+//         code: 'eosio',
+//         scope: 'eosio',
+//         table: 'rexpool',
+//         limit: '1',
+//     });
+
+//     if (!rexPoolResponse || !rexPoolResponse.rows.length) {
+//         console.error('Failed to fetch rexpool');
+//         return;
+//     }
+
+//     const distConfigResponse = await $api.getTableRows({
+//         code: 'exrsrv.tf',
+//         scope: 'exrsrv.tf',
+//         table: 'config',
+//         limit: '1',
+//     });
+
+//     if (!distConfigResponse || !distConfigResponse.rows.length) {
+//         console.error('Failed to fetch exrsrv.tf config');
+//         return;
+//     }
+
+//     const payoutsResponse = await $api.getTableRows({
+//         code: 'exrsrv.tf',
+//         scope: 'exrsrv.tf',
+//         table: 'payouts',
+//         limit: '100',
+//     });
+
+//     if (!payoutsResponse || !payoutsResponse.rows.length) {
+//         console.error('Failed to fetch exrsrv.tf payouts');
+//         return;
+//     }
+
+//     const rexStats = rexPoolResponse.rows[0];
+//     const distConfig = distConfigResponse.rows[0];
+//     const payoutRow = payoutsResponse.rows.find((row) => row.to === 'eosio.rex');
+
+//     const annualPayout = new Big(payoutRow.amount).times(new Big(60 * 60 * 24 * 365).div(payoutRow.interval));
+//     const fixedRatio = new Big(distConfig.ratio).div(100);
+//     const rexTotal = new Big(rexStats.total_lendable.split(' ')[0]);
+//     const stlosTotal = new Big(ethers.utils.formatEther(tvlBn));
+
+//     const balanceRatio = rexTotal.eq(0) ? -1 : stlosTotal.times(fixedRatio).div(rexTotal.add(stlosTotal));
+
+//     if (balanceRatio.eq(0)) {
+//         return '0';
+//     }
+
+//     const rexPayout = annualPayout.div(balanceRatio.plus(1));
+//     const stlosPayout = annualPayout.minus(rexPayout);
+
+//     const apy = stlosPayout.div(stlosTotal).times(100).toFixed(2);
+
+//     return apy;
+// }
 
 
 /**
