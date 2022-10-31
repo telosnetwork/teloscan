a87153e49de044a5442813e6acb9ccb324cdc303
tomtomcrypto <titiomm@gmail.com>
2022-10-31 14:32:04 +0000

    trx value to wei fix

--- src/pages/Transaction.vue
+++ src/pages/Transaction.vue
@@ -85,7 +85,7 @@ export default {
                 return;
             }
             this.trx = trxResponse.data.transactions[0];
-            this.trx.value = BigNumber.from("" + this.trx.value + "");
+            this.trx.value = BigNumber.from(this.trx.value.toString());
             await this.loadContract();
             await this.loadTransfers();
             this.setErrorMessage();
