946670f63ddddd3808ecc0c8b3c2558093993980
Don Peat <don@caleos.io>
2022-11-03 14:49:07 -0500

    fix: remove unnec tvl param

--- src/pages/staking/staking-utils.js
+++ src/pages/staking/staking-utils.js
@@ -33,8 +33,8 @@ export function formatUnstakePeriod(seconds) {
     return `${quantity} ${unit}`;
 }
 
-export async function fetchStlosApy($telosApi, tvl) {
-    return await $telosApi.get('apy/evm',tvl);
+export async function fetchStlosApy($telosApi) {
+    return await $telosApi.get('apy/evm');
 }
 
 /**
