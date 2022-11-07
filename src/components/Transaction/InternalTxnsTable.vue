6f8d24162c2b3608cd767ca35ef3c551884a3e28
tomtomcrypto <titiomm@gmail.com>
2022-11-07 13:04:28 +0000

    added hr toggle

---
+++ src/components/Transaction/InternalTxnsTable.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'InternalTxnsTable',
+    props: {
+        itxs: {
+            type: Array,
+            required: true,
+        },
+        parsedItxs: {
+            type: Object,
+            required: true,
+        },
+    },
+    data () {
+        return {
+
+        }
+    },
+}
+</script>
+<template>
+<div></div>
+</template>
