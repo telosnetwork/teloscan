66ed00bdf51d819801d1b42976f59a6f54631045
tomtomcrypto <titiomm@gmail.com>
2022-11-07 21:51:22 +0000

    removed empty data, refactor FragmentTableRow param names

--- src/components/Transaction/LogsTable.vue
+++ src/components/Transaction/LogsTable.vue
@@ -2,11 +2,11 @@
 <div class="q-pa-md">
     <div class="row">
         <div class="col-12">
-            <logs-table-row
+            <FragmentTableRow
                 v-for="(log, index) in logs"
                 :key="`log-row-${index}`"
-                :log="log"
-                :rawLog="rawLogs[index]"
+                :fragment="log"
+                :rawFragment="rawLogs[index]"
             />
         </div>
         <div v-if="logs.length !== rawLogs.length" class="col-12 u-flex--center">
@@ -17,12 +17,12 @@
 </template>
 
 <script>
-import LogsTableRow from 'components/Transaction/FragmentTableRow'
+import FragmentTableRow from 'components/Transaction/FragmentTableRow'
 
 export default {
     name: 'LogsTable',
     components: {
-        LogsTableRow,
+        FragmentTableRow,
     },
     props: {
         contract : {
