d604096b38364c0f69f0f7bc8cc3785fcd04dece
tomtomcrypto <titiomm@gmail.com>
2022-11-07 13:04:28 +0000

    added hr toggle

--- src/components/Transaction/InternalTxns.vue
+++ src/components/Transaction/InternalTxns.vue
@@ -1,9 +1,12 @@
 <script>
 import JsonViewer from 'vue-json-viewer'
+import InternalTxnsTable from './InternalTxnsTable.vue'
+
 export default {
     name: 'InternalTxns',
     components: {
         JsonViewer,
+        InternalTxnsTable,
     },
     props: {
         itxs: {
@@ -15,14 +18,54 @@ export default {
             required: true,
         },
     },
+    created() {
+
+        this.itxs.forEach(async (itx) => {
+            console.log(itx);
+            this.parsedItxs.push(itx);
+        });
+
+    },
     data () {
-        return {}
+        return {
+            human_readable: true,
+            parsedItxs: [],
+        }
     },
 }
 </script>
-
-<template lang="pug">
-pre
-    //- div() {{ JSON.stringify(itxs, null, 4) }}
-    <json-viewer :value="itxs" theme="jsonViewer"></json-viewer>
+<template>
+<div>
+    <div v-if="itxs.length === 0" class="row">
+        <div class="col-12 u-flex--center">
+            <q-icon class="fa fa-info-circle q-mr-md" size="md" />
+            <h3>No internal transactions found</h3>
+        </div>
+    </div>
+    <div v-else class="row">
+        <div class="col-12 u-flex--center-y">
+            <q-toggle
+                v-model="human_readable"
+                icon="visibility"
+                color="secondary"
+                size="lg"
+            />
+            Human-readable
+        </div>
+        <div class="col-12">
+            <internal-txns-table
+                v-if="human_readable"
+                :rawLogs="itxs"
+                :logs="parsedItxs"
+                :contract="contract"
+            />
+            <json-viewer
+                v-else
+                :value="itxs"
+                theme="custom-theme"
+                class="q-mb-md"
+            />
+        </div>
+    </div>
+</div>
 </template>
