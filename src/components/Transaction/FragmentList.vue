6cd18fba909d4fa41f37c3da61c1fbe04c45892e
tomtomcrypto <titiomm@gmail.com>
2022-11-08 09:26:53 +0000

    added depth, refactored Lists for Logs & Internal Tx

---
+++ src/components/Transaction/FragmentList.vue
@@ -0,0 +1,36 @@
+<script>
+import FragmentListElement from 'components/Transaction/FragmentListElement'
+export default {
+    name: 'FragmentList',
+    components: {
+        FragmentListElement,
+    },
+    props: {
+        fragments: {
+            type: Array,
+            required: true,
+        },
+        parsedFragments: {
+            type: Array,
+            required: true,
+        },
+    },
+}
+</script>
+<template>
+<div class="q-pa-md" :key="parsedFragments.length">
+    <div class="row">
+        <div class="col-12">
+            <FragmentListElement
+                :key="'ltr' + i"
+                v-for="(fragment, i) in fragments"
+                :rawFragment="fragment"
+                :fragment="parsedFragments[i]"
+            />
+        </div>
+        <div v-if="fragments.length !== parsedFragments.length" class="col-12 u-flex--center">
+            <q-spinner size="md" />
+        </div>
+    </div>
+</div>
+</template>
