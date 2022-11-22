134deeccd52dc3934ebc9bb67133071102cfb284
tomtomcrypto <titiomm@gmail.com>
2022-11-22 12:01:49 +0000

    log parsing overrides fix

--- src/lib/Contract.js
+++ src/lib/Contract.js
@@ -99,8 +99,7 @@ export default class Contract {
           parsedLog = this.formatLog(log, parsedLog);
           return parsedLog;
         } catch (e) {
-          let parsedLog = await this.parseEvent(log);
-          return  this.formatLog(log, parsedLog);
+          return await this.parseEvent(log);
         }
       }));
       parsedArray.forEach(parsed => {
@@ -114,7 +113,10 @@ export default class Contract {
 
     return await Promise.all(logsArray.map(async log => {
       let parsedLog = await this.parseEvent(log);
-      parsedLog = this.formatLog(log, parsedLog);
+      if(parsedLog.name && parsedLog.eventFragment && parsedLog.eventFragment.inputs){
+        parsedLog.inputs = parsedLog.eventFragment.inputs;
+      }
+      return parsedLog;
     }))
   }
 
