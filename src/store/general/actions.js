bfa3551de5bb84652ab07482681f02fc10ece7be
Ezra Sowden-Guzman <ezratic@gmail.com>
2022-12-27 17:03:46 -0500

    debug: update ismetamask check again

--- src/store/general/actions.js
+++ src/store/general/actions.js
@@ -2,6 +2,8 @@ import detectEthereumProvider from '@metamask/detect-provider';
 
 export async function fetchBrowserMetaMaskSupport({ commit }) {
     const provider = await detectEthereumProvider({ mustBeMetaMask: true });
+    const isBraveBrowser = (navigator.brave && await navigator.brave.isBrave() || false);
+    const isMetaMask = (provider?.isMetaMask && !isBraveBrowser) ?? false
 
-    commit('setBrowserSupportsMetaMask', provider?.isMetaMask ?? false);
+    commit('setBrowserSupportsMetaMask', isMetaMask);
 }
