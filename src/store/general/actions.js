411d8946924fbb100e0182084629f3329017c1e7
Ezra Sowden-Guzman <ezratic@gmail.com>
2022-12-27 17:06:51 -0500

    debug: update ismetamask check again 2

--- src/store/general/actions.js
+++ src/store/general/actions.js
@@ -2,8 +2,9 @@ import detectEthereumProvider from '@metamask/detect-provider';
 
 export async function fetchBrowserMetaMaskSupport({ commit }) {
     const provider = await detectEthereumProvider({ mustBeMetaMask: true });
-    const isBraveBrowser = (navigator.brave && await navigator.brave.isBrave() || false);
-    const isMetaMask = (provider?.isMetaMask && !isBraveBrowser) ?? false
+    const isBraveBrowserMobile = (navigator.brave && await navigator.brave.isBrave() || false) &&
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent);
+    const isMetaMask = (provider?.isMetaMask && !isBraveBrowserMobile) ?? false
 
     commit('setBrowserSupportsMetaMask', isMetaMask);
 }
