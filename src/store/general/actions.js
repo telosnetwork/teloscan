7b30528d54add4c3c5597a0fbd1cf32e76ef6ac4
Ezra Sowden-Guzman <ezratic@gmail.com>
2022-12-27 17:14:14 -0500

    fix: brave metamask detection

--- src/store/general/actions.js
+++ src/store/general/actions.js
@@ -2,9 +2,12 @@ import detectEthereumProvider from '@metamask/detect-provider';
 
 export async function fetchBrowserMetaMaskSupport({ commit }) {
     const provider = await detectEthereumProvider({ mustBeMetaMask: true });
+
+    // required because brave wallet integration deceptively has provider.isMetaMask === true
+    const mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i;
     const isBraveBrowserMobile = (navigator.brave && await navigator.brave.isBrave() || false) &&
-        /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent);
-    const isMetaMask = (provider?.isMetaMask && !isBraveBrowserMobile) ?? false
+        mobileRegex.test(navigator.userAgent);
+    const isMetaMask = provider?.isMetaMask && !isBraveBrowserMobile;
 
     commit('setBrowserSupportsMetaMask', isMetaMask);
 }
