2ed70fb53e59f97bedbcd9df576f63f3d1921a24
Ezra Sowden-Guzman <ezratic@gmail.com>
2022-12-27 17:00:01 -0500

    debug: update ismetamask check

--- src/store/general/actions.js
+++ src/store/general/actions.js
@@ -2,9 +2,6 @@ import detectEthereumProvider from '@metamask/detect-provider';
 
 export async function fetchBrowserMetaMaskSupport({ commit }) {
     const provider = await detectEthereumProvider({ mustBeMetaMask: true });
-    // eslint-disable-next-line
-    debugger;
 
-
-    commit('setBrowserSupportsMetaMask', !!provider);
+    commit('setBrowserSupportsMetaMask', provider?.isMetaMask ?? false);
 }
