8c99c7e43873136c0857faf5a3f84f5223241ccf
Ezra Sowden-Guzman <ezratic@gmail.com>
2022-12-27 16:56:08 -0500

    debug: add debug

--- src/store/general/actions.js
+++ src/store/general/actions.js
@@ -2,6 +2,8 @@ import detectEthereumProvider from '@metamask/detect-provider';
 
 export async function fetchBrowserMetaMaskSupport({ commit }) {
     const provider = await detectEthereumProvider({ mustBeMetaMask: true });
+    debugger;
+
 
     commit('setBrowserSupportsMetaMask', !!provider);
 }
