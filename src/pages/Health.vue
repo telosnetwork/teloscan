2f74f1b391d00d0fdee7ff4db44560d2cc43b829
tomtomcrypto <titiomm@gmail.com>
2023-01-05 15:03:02 +0000

    added monitor, ref

---
+++ src/pages/Health.vue
@@ -0,0 +1,21 @@
+<script>
+import Endpoints from '../components/Health/Endpoints.vue';
+import Monitor from '../components/Health/Monitor.vue';
+export default {
+    name: 'HealthPage',
+    components: {Endpoints, Monitor},
+};
+</script>
+<template lang="pug">
+.pageContainer.q-pt-xl
+    .homeInfo.q-mb-lg
+        .text-primary.text-h6 RPC Endpoints
+    Endpoints()
+    .homeInfo.q-mb-lg
+        .text-primary.text-h6 Monitor
+    Monitor()
+</template>
+
+<style scoped lang='sass'>
+
+</style>
