4df9c9adce2ef43234c52ced9ef91cdf8ef31ae3
Viterbo <viter.rod@gmail.com>
2023-01-25 16:06:38 -0300

    adding German translation - first draft

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,7 +1,9 @@
 import enUS from 'src/i18n/en-us';
+import deDE from 'src/i18n/de-de';
 import esEs from 'src/i18n/es-es';
 
 export default {
     'en-us': enUS,
+    'de-de': deDE,
     'es-es': esEs,
 };
