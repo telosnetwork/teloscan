bcc5460ca8882b1be6688e2e6afbb5d2de6c200d
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-01-26 13:22:45 -0500

    feat: pug yeet transactions.vue

--- src/pages/Transactions.vue
+++ src/pages/Transactions.vue
@@ -8,11 +8,15 @@ export default {
 };
 </script>
 
-<template lang='pug'>
-q-page.column.justify-center.items-center
-    .row.q-pa-lg
-      .text-h4 {{ $t('pages.explore_transactions') }}
-    .row
-      .col-12.q-px-sm
-        TransactionTable( title="$t('pages.recent_transactions')")
+<template>
+<q-page class="column justify-center items-center">
+    <div class="row q-pa-lg">
+        <div class="text-h4">{{ $t('pages.explore_transactions') }}</div>
+    </div>
+    <div class="row">
+        <div class="col-12 q-px-sm">
+            <TransactionTable title="$t('pages.recent_transactions')"/>
+        </div>
+    </div>
+</q-page>
 </template>
