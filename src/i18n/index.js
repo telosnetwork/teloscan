9d94540f3de2eb7aaa9b1132aae30652b9b3ee7c
tomtomcrypto <titiomm@gmail.com>
2023-02-01 18:07:55 +0000

    added frFr to translation index

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,9 +1,11 @@
 import enUS from 'src/i18n/en-us';
 import esEs from 'src/i18n/es-es';
 import ptBR from 'src/i18n/pt-br';
+import frFR from 'src/i18n/fr-fr';
 
 export default {
     'en-us': enUS,
     'es-es': esEs,
     'pt-br': ptBR,
+    'fr-fr': frFR,
 };
