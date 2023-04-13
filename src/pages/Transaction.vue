03965e012ba66e3334ae4930dcec4bc25924f4a1
tomtomcrypto <titiomm@gmail.com>
2023-04-13 17:44:25 +0000

    fix

--- src/pages/Transaction.vue
+++ src/pages/Transaction.vue
@@ -115,8 +115,10 @@ export default {
                 return;
             }
             this.trx = trxResponse.data.results[0];
-            this.trx.logs = this.trx.logs?.replace('transaction_hash', 'transactionHash');
-            this.trx.logs = JSON.parse(this.trx.logs);
+            if(this.trx.logs){
+                this.trx.logs = this.trx.logs.replace('transaction_hash', 'transactionHash');
+                this.trx.logs = JSON.parse(this.trx.logs);
+            }
             this.trx.gasUsed = BigNumber.from(this.trx.gasUsed);
             this.trx.gasLimit = BigNumber.from(this.trx.gasLimit);
             this.trx.value = BigNumber.from(this.trx.value.toLocaleString('fullwide', { useGrouping:false }));
