bb892a7229f038f16ed08af9afc33581f6cf8061
Don Peat <don@caleos.io>
2023-04-13 21:29:30 -0500

    refactor: remove debuggers

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -178,7 +178,6 @@ export function getClientIsApple() {
  * @return {string}
  */
 export function getFormattedUtcOffset(date) {
-    debugger;
     const pad = value => value < 10 ? '0' + value : value;
     const sign = (date.getTimezoneOffset() > 0) ? '-' : '+';
     const offset = Math.abs(date.getTimezoneOffset());
