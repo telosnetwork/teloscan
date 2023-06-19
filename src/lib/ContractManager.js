6d0a03d1e5f2c81445ff93089dddaa4fbdd3c149
Don Peat <don@caleos.io>
2023-06-19 12:39:55 -0500

    chore: upgrade web3 to 4.0.1
    refactor: adjust syntax for web3 imports and usage

--- src/lib/ContractManager.js
+++ src/lib/ContractManager.js
@@ -2,7 +2,7 @@ import Contract from 'src/lib/Contract';
 import { ethers } from 'ethers';
 import functions_overrides from 'src/lib/abi/signature/functions_signatures_overrides.json';
 import events_overrides from 'src/lib/abi/signature/events_signatures_overrides.json';
-import Web3 from 'web3';
+import { Web3 } from 'web3';
 import axios from 'axios';
 import erc20Abi from 'erc-20-abi';
 import { erc721Abi, erc1155Abi, erc721MetadataAbi, supportsInterfaceAbi } from 'src/lib/abi';
