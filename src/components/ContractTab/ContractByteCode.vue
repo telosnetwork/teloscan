32645c4294cf5f4751dee1427e5b2c26c58e347f
tomtomcrypto <titiomm@gmail.com>
2023-07-17 17:11:08 +0000

    bytecode css

--- src/components/ContractTab/ContractByteCode.vue
+++ src/components/ContractTab/ContractByteCode.vue
@@ -41,15 +41,21 @@ export default {
 <template>
 <div class="q-pt-lg q-pb-md">
     <q-spinner v-if="loading" size="md" />
-    <div v-else>
+    <div v-else class="bytecode shadow-2">
         <pre>{{ bytecode }}</pre>
     </div>
 </div>
 </template>
 
 <style scoped lang="sass">
+.bytecode
+    padding: 10px 15px 10px 15px
+    background: rgba(103, 110, 203, 0.08)
+.body--dark .bytecode
+    background: rgba(47, 60, 156, 0.11)
 pre
     white-space: unset
     max-width: 100%
+    margin: 0
     word-break: break-all
 </style>
