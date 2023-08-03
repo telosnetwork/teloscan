206d778659172316229e7ada89f3fd04274e0477
Jesse Schulman <jesse@caleos.io>
2023-08-02 22:54:21 -0700

    Disable SSR function for now

--- functions/testing.js
+++ functions/testing.js
@@ -4,8 +4,11 @@
 
 
 export function onRequest(context) {
+    /*
     const env = require('../public/env')(context);
     console.dir(context);
     const apiEndpoint = env.NETWORK_EVM_RPC;
     return new Response(`Hello, ${context.env.NETWORK} world!!!!!!!!\n\nNetwork RPC is: ${apiEndpoint}`);
+     */
+    return new Response(`Hello, world!`);
 }
