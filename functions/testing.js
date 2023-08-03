be81565da5729244640924470f20f1c758c4f012
Jesse Schulman <jesse@caleos.io>
2023-08-02 22:59:53 -0700

    Fix syntax for linting

--- functions/testing.js
+++ functions/testing.js
@@ -3,12 +3,12 @@
 //   - change the CI workflow so it copies only the .js files
 
 
-export function onRequest(context) {
+export function onRequest() {
     /*
     const env = require('../public/env')(context);
     console.dir(context);
     const apiEndpoint = env.NETWORK_EVM_RPC;
     return new Response(`Hello, ${context.env.NETWORK} world!!!!!!!!\n\nNetwork RPC is: ${apiEndpoint}`);
      */
-    return new Response(`Hello, world!`);
+    return new Response('Hello, world!');
 }
