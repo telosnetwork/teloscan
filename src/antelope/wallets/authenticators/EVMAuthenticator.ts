178251658f254a0902058accafa31d675db282e6
Ezra Sowden-Guzman <ezratic@gmail.com>
2023-10-11 14:55:21 -0400

    update lock file, cleanup imports

--- src/antelope/wallets/authenticators/EVMAuthenticator.ts
+++ src/antelope/wallets/authenticators/EVMAuthenticator.ts
@@ -4,14 +4,27 @@
 
 import { SendTransactionResult, WriteContractResult } from '@wagmi/core';
 import { BigNumber, ethers } from 'ethers';
-import { createTraceFunction } from 'src/antelope/mocks/FeedbackStore';
-import { CURRENT_CONTEXT, getAntelope, useAccountStore } from 'src/antelope/mocks';
-import { EVMChainSettings } from 'src/antelope/mocks';
-import { useChainStore } from 'src/antelope/mocks';
-import { useEVMStore } from 'src/antelope/mocks';
-import { isTracingAll, useFeedbackStore } from 'src/antelope/mocks';
-import { usePlatformStore } from 'src/antelope/mocks';
-import { AntelopeError, EvmABI, EvmFunctionParam, EvmTransactionResponse, ExceptionError, TokenClass, addressString } from 'src/antelope/wallets/types';
+import {
+    CURRENT_CONTEXT,
+    getAntelope,
+    useAccountStore,
+    EVMChainSettings,
+    useChainStore,
+    useEVMStore,
+    isTracingAll,
+    usePlatformStore,
+    createTraceFunction,
+    useFeedbackStore,
+} from 'src/antelope/mocks';
+import {
+    AntelopeError,
+    EvmABI,
+    EvmFunctionParam,
+    EvmTransactionResponse,
+    ExceptionError,
+    TokenClass,
+    addressString,
+} from 'src/antelope/wallets/types';
 
 export abstract class EVMAuthenticator {
 
