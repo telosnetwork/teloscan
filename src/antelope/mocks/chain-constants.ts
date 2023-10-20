c509fb112466861b8296ce2900177c33350dd4cd
pmjanus <97489909+pmjanus@users.noreply.github.com>
2023-10-20 14:55:25 -0500

    Update chain-constants.ts

--- src/antelope/mocks/chain-constants.ts
+++ src/antelope/mocks/chain-constants.ts
@@ -1,14 +1,14 @@
 export const TELOS_CHAIN_IDS = ['40', '41'];
 export const TELOS_NETWORK_NAMES = ['telos-evm', 'telos-evm-testnet'];
 export const TELOS_ANALYTICS_EVENT_IDS = {
-    loginStarted: 'JXIYBP1S',
-    loginSuccessful: 'HIP11SFR',
-    loginSuccessfulMetamask: 'ABGMND23',
-    loginFailedMetamask: 'ZFGE6TFS',
-    loginSuccessfulSafepal: 'LKCBKDU2',
-    loginFailedSafepal: '6PSIWGNV',
-    loginSuccessfulOreId: 'MSXMDDNK',
-    loginFailedOreId: 'UFX3JYRJ',
-    loginFailedWalletConnect: '9V4IV1BV',
-    loginSuccessfulWalletConnect: '2EG2OR3H',
+    loginStarted: 'GXQ8FOAU',
+    loginSuccessful: 'B1BZBK5R',
+    loginSuccessfulMetamask: 'JQTSKEFY',
+    loginFailedMetamask: 'PKHBRTYV',
+    loginSuccessfulSafepal: 'WV5OSVMQ',
+    loginFailedSafepal: '7WSBR8LA',
+    loginSuccessfulOreId: 'HUDIQQEK',
+    loginFailedOreId: 'AJXWOXWJ',
+    loginFailedWalletConnect: 'ZVKXJDK1',
+    loginSuccessfulWalletConnect: 'AQMOMICF',
 };
