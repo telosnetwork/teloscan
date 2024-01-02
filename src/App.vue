ea109ce2302abb517ed876eb046814ec165b6420
Ezra Sowden-Guzman <ezratic@gmail.com>
2024-01-02 16:58:54 -0500

    only load fathom for telos

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <script lang="ts">
-import { getAntelope } from 'src/antelope';
 import { defineComponent } from 'vue';
+
+import { getAntelope, useChainStore } from 'src/antelope';
+import { TELOS_NETWORK_NAMES } from 'src/antelope/mocks/chain-constants';
 
 export const isTodayBeforeTelosCloudDown = new Date().getTime() < new Date('2023-12-31').getTime();
 
 export default defineComponent({
     name: 'App',
     mounted() {
-        const script = document.createElement('script');
-        script.src = 'https://cdn.usefathom.com/script.js';
-        script.dataset.site = 'PDKJSBKL';
-        script.dataset.spa = 'auto';
-        script.defer = true;
-        document.body.appendChild(script);
+        const network = useChainStore().currentChain.settings.getNetwork();
+        if (TELOS_NETWORK_NAMES.includes(network)) {
+            const script = document.createElement('script');
+            script.src = 'https://cdn.usefathom.com/script.js';
+            script.dataset.site = 'PDKJSBKL';
+            script.dataset.spa = 'auto';
+            script.defer = true;
+            document.body.appendChild(script);
+        }
 
         if (isTodayBeforeTelosCloudDown) {
             getAntelope().config.notifyRememberInfoHandler(
