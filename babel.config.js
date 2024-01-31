118a7a8a8e94096b76e54ca08f043507d7869ddb
Ezra Sowden-Guzman <ezratic@gmail.com>
2024-01-31 14:55:25 -0500

    remove old files

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    presets: [
-        '@quasar/babel-preset-app',
-        ['@babel/preset-env', { targets: { node: 'current' } }],
-        '@babel/preset-typescript',
-    ],
-};
