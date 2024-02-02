b889a64956febd2358b2f44cebe22da6ab5808ee
Ezra Sowden-Guzman <ezratic@gmail.com>
2024-02-01 20:08:27 -0500

    WIP handle proxy contracts

--- src/components/ContractTab/ContractInterface.vue
+++ src/components/ContractTab/ContractInterface.vue
@@ -11,13 +11,33 @@ export default {
             type: Boolean,
             required: true,
         },
+        proxy: {
+            type: String,
+            default: '',
+        },
+        showProxyOwnFunctions: {
+            type: Boolean,
+            default: false,
+        },
     },
     data: () => ({
-        functions: [],
-        contract: [],
+        functions: {
+            read: [],
+            write: [],
+        },
+        contract: {},
+        isProxyForUnverifiedContract: false,
     }),
     async mounted() {
-        this.contract = await this.$contractManager.getContract(this.$route.params.address);
+        // eztodo add label saying it is a proxy if it is
+        const contractAddress = this.showProxyOwnFunctions ? this.proxy : this.$route.params.address;
+        this.contract = await this.$contractManager.getContract(contractAddress);
+
+        if (this.proxy && !this.showProxyOwnFunctions && !this.contract.abi) {
+            this.isProxyForUnverifiedContract = true;
+            return;
+        }
+
         let read = [];
         let write = [];
         this.contract.abi.forEach((a) => {
@@ -42,7 +62,25 @@ export default {
 
 <template>
 <div class="q-pa-md">
-    <q-list>
+    <!-- eztodo i18n -->
+    <p v-if="proxy && !showProxyOwnFunctions">
+        This contract is an EIP-1967 Transparent Proxy for the contract <router-link :to="{ name: 'address', params: { address: proxy } }">{{ proxy }}</router-link>.
+    </p>
+
+    <!-- eztodo i18n -->
+    <template v-if="isProxyForUnverifiedContract">
+        <p class="q-mt-md">
+            The contract for which this is a proxy is not verified, so its ABI is not available. To interact with the implementation contract,
+            visit the <router-link :to="{ name: 'address', params: { address: proxy } }">implementation contract page</router-link>.
+        </p>
+    </template>
+
+    <!-- eztodo i18n -->
+    <p v-if="(this.write && !this.functions.write.length) || (this.read && !this.functions.read.length)" class="q-mt-md">
+        There are no available Contract ABI methods to read
+    </p>
+
+    <q-list v-if="(this.write && this.functions.write.length) || (this.read && this.functions.read.length)">
         <q-expansion-item
             v-for="func in (write ? functions.write : functions.read)"
             :key="func.name"
@@ -59,7 +97,6 @@ export default {
                     />
                 </div>
             </q-card>
-
         </q-expansion-item>
     </q-list>
 </div>
--- src/components/ContractTab/ContractTab.vue
+++ src/components/ContractTab/ContractTab.vue
@@ -1,7 +1,17 @@
 <script>
+import Web3 from 'web3';
+
 import ContractSource from 'components/ContractTab/ContractSource';
 import ContractInterface from 'components/ContractTab/ContractInterface';
 import CopyButton from 'components/CopyButton.vue';
+
+const TABS = {
+    source: 'source',
+    read: 'read',
+    write: 'write',
+    proxyRead: 'proxyRead',
+    proxyWrite: 'proxyWrite',
+};
 
 export default {
     name: 'ContractTab',
@@ -17,9 +27,30 @@ export default {
         },
     },
     data: () => ({
-        source: true,
-        write: false,
+        selectedTab: TABS.source,
+        loading: true,
+        proxyContractAddress: '',
+        TABS,
     }),
+    async mounted() {
+        const contractAddress = this.contract.address;
+
+        const web3 = new Web3(process.env.NETWORK_EVM_RPC);
+
+        // Slot for the implementation address
+        const implementationSlot = '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc';
+
+        async function checkEIP1967Implementation() {
+            const implementationAddressBytes = await web3.eth.getStorageAt(contractAddress, implementationSlot);
+            // Extract the last 40 characters (20 bytes) to get the actual address
+            const implementationAddress = implementationAddressBytes !== '0x0' ? '0x' + implementationAddressBytes.slice(-40) : null;
+
+            return implementationAddress ?? '';
+        }
+
+        this.proxyContractAddress = await checkEIP1967Implementation(this);
+        this.loading = false;
+    },
     computed: {
         abi() {
             const { abi } = this.contract;
@@ -30,55 +61,76 @@ export default {
 
             return JSON.stringify(this.contract.abi);
         },
-        codeSeleted() {
-            return this.source === true;
-        },
-        readSelected() {
-            return this.source === false && this.write === false;
-        },
-        writeSelected() {
-            return this.source === false && this.write === true;
+        contractIsProxy() {
+            return !!this.proxyContractAddress;
         },
     },
 };
 </script>
 
 <template>
-<div class="contract-tab">
-    <CopyButton
-        v-if="abi"
-        :text="abi"
-        :accompanying-text="$t('components.contract_tab.copy_abi_to_clipboard')"
-        class="q-mb-md"
-    />
-    <br>
+<div class='contract-tab'>
+    <div v-if='loading' class='text-center'>
+        <q-spinner
+            size='md'
+            class='q-ma-xl'
+        />
+    </div>
 
-    <q-btn-group>
-        <q-btn
-            :outline="codeSeleted"
-            :label="$t('components.contract_tab.code')"
-            push
-            @click="source = true"
+    <template v-else>
+        <CopyButton
+            v-if='abi'
+            :text='abi'
+            :accompanying-text="$t('components.contract_tab.copy_abi_to_clipboard')"
+            class='q-mb-md'
         />
-        <q-btn
-            :outline="readSelected"
-            :label="$t('components.contract_tab.read')"
-            push
-            @click="source = false; write = false"
-        />
-        <q-btn
-            :outline="writeSelected"
-            :label="$t('components.contract_tab.write')"
-            push
-            @click="source = false; write = true"
-        />
-    </q-btn-group>
 
-    <ContractSource v-if="source" />
-    <ContractInterface
-        v-else
-        :write="write"
-    />
+        <br>
+
+        <q-btn-group>
+            <q-btn
+                :outline="selectedTab === TABS.source"
+                :label="$t('components.contract_tab.code')"
+                push
+                @click='selectedTab = TABS.source'
+            />
+            <q-btn
+                :outline="selectedTab === TABS.read"
+                :label="$t('components.contract_tab.read')"
+                push
+                @click="selectedTab = TABS.read"
+            />
+            <q-btn
+                :outline="selectedTab === TABS.write"
+                :label="$t('components.contract_tab.write')"
+                push
+                @click="selectedTab = TABS.write"
+            />
+            <!-- eztodo i18n -->
+            <q-btn
+                v-if="contractIsProxy"
+                :outline="selectedTab === TABS.proxyRead"
+                :label="'Read as Proxy'"
+                push
+                @click="selectedTab = TABS.proxyRead"
+            />
+            <q-btn
+                v-if="contractIsProxy"
+                :outline="selectedTab === TABS.proxyWrite"
+                :label="'Write as Proxy'"
+                push
+                @click="selectedTab = TABS.proxyWrite"
+            />
+        </q-btn-group>
+
+        <ContractSource v-if="selectedTab === TABS.source" />
+        <ContractInterface
+            v-else
+            :write="[TABS.write, TABS.proxyWrite].includes(selectedTab)"
+            :showProxyOwnFunctions="[TABS.read, TABS.write].includes(selectedTab)"
+            :proxy="proxyContractAddress"
+        />
+    </template>
 </div>
 </template>
 
