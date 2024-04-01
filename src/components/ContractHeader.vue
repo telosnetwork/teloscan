403151d61b8328204799158b40c27e1481c07dd8
Viterbo <viter.rod@gmail.com>
2024-04-01 19:17:28 -0300

    removing commented code

--- src/components/ContractHeader.vue
+++ src/components/ContractHeader.vue
@@ -18,68 +18,7 @@ defineProps({
 
 <template>
 <div class="c-contract-header">
-    <!--
-        <q-card class="c-contract-header c-contract-header__title">
-            <q-card-section v-if="contract.isVerified()" class="c-contract-header__section c-contract-header__section--bold">
-                <q-icon
-                    class="fas fa-check-circle text-positive c-contract-header__icon"
-                    size=".75rem"
-                >
-                    <q-tooltip>
-                        {{ $t('components.contract_tab.verified_contract') }}
-                    </q-tooltip>
-                </q-icon>
-                Contract Source Code Verified
-            </q-card-section>
-            <q-card-section v-else class="c-contract-header__section">
-                <q-icon
-                    class="cursor text-warning c-contract-header__icon"
-                    name="warning"
-                    size=".75rem"
-                >
-                    <q-tooltip>
-                        {{ $t('components.contract_tab.unverified_contract') }}
-                    </q-tooltip>
-                </q-icon>
-                Unverified Contract. Verify and Publish with
-                <a href="https://sourcify.dev/#/verifier">
-                    Sourcify
-                </a>.
-            </q-card-section>
-        </q-card>
-        <q-card class="c-contract-header">
-            <q-card-section class="c-contract-header__section">
-                Contract Name:
-                <div class="c-contract-header__section--bold c-contract-header__section--inline">
-                    {{ contract.getName() }}
-                </div>
-            </q-card-section>
-            <q-card-section class="c-contract-header__section">
-                Compiler Version:
-                <div class="c-contract-header__section--bold c-contract-header__section--inline">
-                    {{ metaData?.compiler }}
-                </div>
-            </q-card-section>
-        </q-card>
-        <q-card class="c-contract-header">
-            <q-card-section class="c-contract-header__section">
-                Optimization Enabled:
-                <div class="c-contract-header__section--bold c-contract-header__section--inline">
-                    {{ metaData?.enabled }}
-                    <span class="c-contract-header__section--regular">&nbsp;with&nbsp;</span>
-                    {{ metaData?.runs }}
-                    <span class="c-contract-header__section--regular">&nbsp;runs</span>
-                </div>
-            </q-card-section>
-        </q-card>
 
-        <div>
-            <div class="c-contract-header__section">
-                <q-icon name="far fa-file-code" size=".75rem" class="c-contract-header__icon" />
-                Contract Source Code
-            </div>
-        </div>
-    -->
     <div class="c-contract-header__title">
         <q-icon
             class="fas fa-check-circle text-positive c-contract-header__icon"
