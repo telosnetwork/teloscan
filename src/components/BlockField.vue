8881517227c39bcda6985037805a692d300dba97
donnyquixotic <don@caleos.io>
2024-04-01 11:02:43 -0500

    refactor: blockfield

--- src/components/BlockField.vue
+++ src/components/BlockField.vue
@@ -1,17 +1,14 @@
-<script>
-export default {
-    name: 'BlockField',
-    props: {
-        block: {
-            type: Number,
-            required: true,
-        },
+<script setup lang="ts">
+defineProps({
+    block: {
+        type: Number,
+        required: true,
     },
-};
+});
 </script>
 
 <template>
-<RouterLink :key="$route.path" :to="`/block/${this.block}`">
+<RouterLink :key="$route.path" :to="`/block/${block}`">
     {{ block }}
 </RouterLink>
 </template>
