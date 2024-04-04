4ccc451d9aec67471588c4c3d062aa4915e165d8
Viterbo Rodríguez <viter.rod@gmail.com>
2024-04-04 14:44:06 -0300

    adding erc271 and erc1155 transfers tabs

--- src/components/TransferTable.vue
+++ src/components/TransferTable.vue
@@ -88,6 +88,7 @@ export default {
             },
             showDateAge: true,
             tokenList: {},
+            highlightAddress: '',
         };
     },
     created(){
@@ -113,10 +114,22 @@ export default {
         });
     },
     methods: {
+        truncatedId(id) {
+            // only truncate if the id is longer than 7 characters
+            // we need to show the first 4 and last 3 characters
+            if (id.length > 7) {
+                return id.substr(0, 4) + '...' + id.substr(id.length - 3);
+            } else {
+                return id;
+            }
+        },
+        setHighlightAddress(address) {
+            this.highlightAddress = address;
+        },
         async updateLoadingRows() {
-            this.loadingRows.value = [];
-            for (var i = 1; i <= this.pagination.value.rowsPerPage; i++) {
-                this.loadingRows.value.push(i);
+            this.loadingRows = [];
+            for (var i = 1; i <= this.pagination.rowsPerPage; i++) {
+                this.loadingRows.push(i);
             }
         },
         async onRequest(props) {
@@ -129,8 +142,6 @@ export default {
                 this.pagination.rowsNumber = response.data.total_count;
             }
 
-            const tokenList = await this.$contractManager.getTokenList();
-
             this.pagination.page = page;
             this.pagination.rowsPerPage = rowsPerPage;
             this.pagination.sortBy = sortBy;
@@ -139,40 +150,32 @@ export default {
 
             let newTransfers = [];
             for (const transfer of response.data.results) {
-                let contract = await this.$contractManager.getContract(transfer.contract);
+                const contractData = response.data.contracts[transfer.contract];
                 let valueDisplay;
                 if(this.tokenType === 'erc20'){
-                    if (contract && contract.properties.decimals) {
-                        valueDisplay = formatWei(transfer.amount, contract.properties.decimals);
+                    if (contractData && contractData.decimals) {
+                        valueDisplay = formatWei(transfer.amount, contractData.decimals);
                     } else {
                         valueDisplay = this.$t('components.unknown_precision');
                     }
                 } else if (this.tokenType === 'erc721') {
                     valueDisplay = '#' + transfer.id.toString();
-                    if(contract){
-                        transfer.token = await this.$contractManager.loadNFT(contract, transfer.id);
-                    }
                 } else if (this.tokenType === 'erc1155') {
                     valueDisplay = transfer.amount.toString();
                 } else if(transfer.id) {
                     valueDisplay = '#' + transfer.id.toString();
                 }
-                tokenList.tokens.forEach((token) => {
-                    if(token.address.toLowerCase() === transfer.contract.toLowerCase()){
-                        contract.logoURI = token.logoURI;
-                    }
-                });
+
                 const nTransfer = {
                     hash: transfer.transaction,
                     timestamp: transfer.timestamp,
-                    count: 1,
+                    count: transfer.amount,
                     id: transfer.id,
                     token: transfer.token,
                     value: valueDisplay,
-                    contract: contract,
+                    contract: contractData,
                     from: toChecksumAddress(transfer.from),
                     to: toChecksumAddress(transfer.to),
-                    ...contract,
                 };
 
                 newTransfers.push(nTransfer);
@@ -278,10 +281,24 @@ export default {
                 </span>
             </q-td>
             <q-td key="from" :props="props">
-                <AddressField :key="props.row.from" :address="props.row.from" :truncate="16"/>
+                <AddressField
+                    v-if="props.row.from"
+                    :key="props.row.from"
+                    :address="props.row.from"
+                    :truncate="16"
+                    :highlightAddress="highlightAddress"
+                    @highlight="setHighlightAddress"
+                />
             </q-td>
             <q-td key="to" :props="props">
-                <AddressField :key="props.row.to" :address="props.row.to" :truncate="16"/>
+                <AddressField
+                    v-if="props.row.to"
+                    :key="props.row.to"
+                    :address="props.row.to"
+                    :truncate="16"
+                    :highlightAddress="highlightAddress"
+                    @highlight="setHighlightAddress"
+                />
             </q-td>
             <q-td key="value" :props="props">
                 <span v-if="tokenType==='erc721' && props.row.token?.imageCache">
@@ -301,13 +318,16 @@ export default {
             </q-td>
             <q-td key="token" :props="props" class="flex items-center">
                 <AddressField
-                    :key="props.row.contract"
-                    class="token-name"
-                    :address="props.row.address"
-                    :contract="props.row.contract"
-                    :truncate="15"
+                    :key="props.row.contract.address"
+                    :address="props.row.contract.address"
+                    :truncate="16"
+                    :highlightAddress="highlightAddress"
+                    @highlight="setHighlightAddress"
                 />
-                <span v-if="tokenType === 'erc1155'" class="q-pl-xs" >#{{ props.row.id}}</span>
+                <span v-if="tokenType === 'erc1155'" class="q-pl-xs" >
+                    #{{ truncatedId(props.row.id) }}
+                    <q-tooltip>{{ props.row.id }}</q-tooltip>
+                </span>
             </q-td>
         </q-tr>
     </template>
@@ -315,7 +335,7 @@ export default {
 <q-table
     v-else
     v-model:pagination="pagination"
-    :rows="rows"
+    :rows="loadingRows"
     :row-key="row => row.hash"
     :columns="columns"
     :rows-per-page-label="$t('global.records_per_page')"
@@ -372,7 +392,7 @@ export default {
             <q-td key="value" >
                 <q-skeleton type="text" class="c-trx-overview__skeleton" />
             </q-td>
-            <q-td key="token"  class="flex items-center">
+            <q-td key="token" >
                 <q-skeleton type="text" class="c-trx-overview__skeleton" />
             </q-td>
         </q-tr>
