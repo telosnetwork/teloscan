aff8a5c58be68a0570e4ef8e3c66e09bebe69a6f
Viterbo <viter.rod@gmail.com>
2024-04-05 16:47:24 -0300

    adding method column

--- src/components/NftTransfersTable.vue
+++ src/components/NftTransfersTable.vue
@@ -1,23 +1,68 @@
+<!-- eslint-disable no-case-declarations -->
 <script lang="ts" setup>
 import { onMounted, ref, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 
-import { indexerApi } from 'src/boot/telosApi';
+import { contractManager, indexerApi } from 'src/boot/telosApi';
 
 import TransactionField from 'components/TransactionField';
 import AddressField from 'components/AddressField';
+import MethodField from 'components/MethodField';
 import DateField from 'components/DateField.vue';
 import { formatWei, toChecksumAddress } from 'src/lib/utils';
-import { Pagination } from 'src/types';
+import { EvmTransactionExtended, Pagination } from 'src/types';
+import { EvmContractFunctionParameter, EvmTransaction, EvmTransactionLog } from 'src/antelope/types';
+import { ethers } from 'ethers';
 
 const { t: $t } = useI18n();
+
+// ---------------------
+interface TransfersResponse {
+    code: number;
+    success: boolean;
+    total_count: number;
+    more: boolean;
+    message: string;
+    contracts: { [address: string]: ContractData };
+    results: TransferData[];
+}
+
+interface ContractData {
+    symbol?: string;
+    creator?: string;
+    address: string;
+    fromTrace?: boolean;
+    trace_address?: string;
+    supply?: string;
+    calldata?: string;
+    block_timestamp?: number;
+    decimals?: number | null;
+    name?: string;
+    block?: number;
+    supportedInterfaces?: string[];
+    transaction?: string;
+}
+
+interface TransferData {
+    amount: string; // amount of tokens
+    logIndex: number;
+    contract: string;
+    blockNumber: number;
+    from: string;
+    id: string;
+    to: string;
+    type: string;
+    transaction: string;
+    timestamp: string;
+}
+
+// ---------------------
 
 export interface NftTransferData {
     hash: string;
     timestamp: number;
-    count: string;
+    amount: string;
     id: string;
-    token: string;
     value: string;
     contract: {
         address: string;
@@ -27,6 +72,7 @@ export interface NftTransferData {
     };
     from: string;
     to: string;
+    trx: EvmTransactionExtended | null;
 }
 
 export interface NftTransferProps {
@@ -48,6 +94,7 @@ const loading = ref(false);
 const loadingRows = ref<Array<number>>([]);
 const loadingCols = ref<Array<number>>([]);
 const showDateAge = ref(true);
+const highlightMethod = ref('');
 
 const pagination = ref<Pagination>(
     {
@@ -63,6 +110,11 @@ const columns = [
     {
         name: 'hash',
         label: $t('components.tx_hash'),
+        align: 'left',
+    },
+    {
+        name: 'method',
+        label: $t('components.method'),
         align: 'left',
     },
     {
@@ -86,13 +138,28 @@ const columns = [
         align: 'left',
     },
     {
+        name: 'id',
+        label: $t('components.nfts.id'),
+        align: 'left',
+    },
+    {
         name: 'value',
         label: $t('components.value'),
         align: 'left',
     },
     {
+        name: 'amount',
+        label: $t('components.amount'),
+        align: 'left',
+    },
+    {
         name: 'token',
         label: $t('components.token'),
+        align: 'left',
+    },
+    {
+        name: 'item',
+        label: $t('components.item'),
         align: 'left',
     },
 ] as {
@@ -101,9 +168,13 @@ const columns = [
     align: string,
 }[];
 
+function setHighlightMethod(val: string) {
+    highlightMethod.value = val;
+}
+
 const truncatedId = (id: string) => {
     if (id.length > 7) {
-        return `${id.substr(0, 4)}...${id.substr(id.length - 3)}`;
+        return `${id.substring(0, 4)}...${id.substring(id.length - 3)}`;
     } else {
         return id;
     }
@@ -126,12 +197,97 @@ const getPath = (settings: { pagination: Pagination }) => {
     return path;
 };
 
+// Method name resolution ------------------
+// This must be refactored in the future. Follow issue #654
+// https://github.com/telosnetwork/teloscan/issues/654
+const resolveMethodName = async (hash: string) => {
+    try {
+        const trxResponse = await indexerApi.get(`/transaction/${hash}?full=true&includeAbi=true`);
+        if (trxResponse.data.results.length === 0) {
+            console.error(`Transaction ${hash} not found`);
+            return;
+        }
+        const aux = trxResponse.data.results[0] as EvmTransaction;
+        let logsArray: EvmTransactionLog[] = [];
+        if(aux.logs){
+            const fixedStr = aux.logs.replace('transaction_hash', 'transactionHash');
+            try {
+                logsArray = JSON.parse(fixedStr) as EvmTransactionLog[];
+            } catch (e) {
+                console.error('Error parsing logs', e);
+            }
+        }
+        const _trx:EvmTransactionExtended = {
+            ...aux,
+            gasUsedBn: ethers.BigNumber.from(aux.gasUsed),
+            gasLimitBn: ethers.BigNumber.from(aux.gasLimit),
+            valueBn: ethers.BigNumber.from(aux.value),
+            gasPriceBn: ethers.BigNumber.from(aux.gasPrice),
+            contract: undefined,
+            parsedTransaction: undefined,
+            functionParams: [],
+            logsArray,
+        };
+        await loadContract(_trx);
+    } catch (e) {
+        console.error('Error resolving method name', e);
+        return;
+    }
+};
+const loadContract = async (_trx: EvmTransactionExtended) => {
+    const transfer = rows.value.find(t => t.hash === _trx.hash && t.trx === null);
+    if (!_trx || _trx.input === '0x') {
+        if (transfer) {
+            transfer.trx = _trx;
+        }
+        return;
+    }
+    _trx.contract = await contractManager.getContract(_trx.to?.toLowerCase());
+    if (!_trx.contract) {
+        if (transfer) {
+            transfer.trx = _trx;
+        }
+        return;
+    }
+
+    _trx.parsedTransaction = await contractManager.parseContractTransaction(
+        _trx,
+        _trx.input,
+        _trx.contract,
+    );
+
+    _trx.functionParams = getFunctionParams(_trx);
+
+    if (transfer) {
+        transfer.trx = _trx;
+    }
+};
+const getFunctionParams = (trx: EvmTransactionExtended) => {
+    if (!trx.parsedTransaction) {
+        return [];
+    }
+    const args:EvmContractFunctionParameter[] = [];
+    trx.parsedTransaction.functionFragment.inputs.forEach((input, i) => {
+        args.push({
+            name: input.name,
+            type: input.type,
+            arrayChildren: (input.arrayChildren !== null) ? input.arrayChildren.type : false,
+            value:  trx.parsedTransaction?.args[i],
+        });
+    });
+    return args;
+};
+// ----------------------------------------------------
+
+
+
+
 const onRequest = async (settings: { pagination: Pagination}) => {
     loading.value = true;
 
     const { page, rowsPerPage, sortBy, descending } = settings.pagination;
 
-    let response = await indexerApi.get(getPath(settings));
+    let response = await indexerApi.get(getPath(settings)) as { data: TransfersResponse };
     if (!pagination.value.rowsNumber && response.data?.total_count) {
         pagination.value.rowsNumber = response.data.total_count;
     }
@@ -144,6 +300,7 @@ const onRequest = async (settings: { pagination: Pagination}) => {
 
     let newTransfers = [] as NftTransferData[];
     for (const transfer of response.data.results) {
+        resolveMethodName(transfer.transaction);
         const contractData = response.data.contracts[transfer.contract];
         let valueDisplay;
         if(props.tokenType === 'erc20'){
@@ -162,14 +319,14 @@ const onRequest = async (settings: { pagination: Pagination}) => {
 
         const nTransfer = {
             hash: transfer.transaction,
-            timestamp: transfer.timestamp,
-            count: transfer.amount,
+            timestamp: new Date(transfer.timestamp).getTime(),
+            amount: transfer.amount,
             id: transfer.id,
-            token: transfer.token,
             value: valueDisplay,
             contract: contractData,
             from: toChecksumAddress(transfer.from),
             to: toChecksumAddress(transfer.to),
+            trx: null,
         } as NftTransferData;
 
         newTransfers.push(nTransfer);
@@ -205,20 +362,28 @@ const updateLoadingRows = () => {
 
 const updateCols = () => {
     console.log('updateCols()', props.tokenType);
+    let exclude = [] as string[];
+
     switch (props.tokenType) {
     case 'erc20':
-        columns[5].label = $t('components.value');
+        exclude = ['id', 'amount', 'item'];
         break;
     case 'erc721':
-        columns[5].label = $t('components.nfts.id');
+        exclude = ['value', 'amount', 'item'];
         break;
     case 'erc1155':
-        columns[5].label = $t('components.nfts.amount');
+        exclude = ['value', 'item'];
         break;
     default:
         throw new Error($t('components.unsupported_token_type', { tokenType: props.tokenType }));
     }
 
+    const filtered = columns.filter(col => !exclude.includes(col.name));
+    columns.splice(
+        0,
+        columns.length,
+        ...filtered,
+    );
     loadingCols.value = [];
     for (var i = 1; i <= columns.length; i++) {
         loadingCols.value.push(i);
@@ -294,6 +459,16 @@ onMounted(() => {
             <q-td key="hash" :props="props">
                 <TransactionField :transaction-hash="props.row.hash"/>
             </q-td>
+            <q-td key="method" :props="props">
+                <MethodField
+                    v-if="props.row.trx"
+                    :trx="props.row.trx"
+                    :shortenName="true"
+                    :highlightMethod="highlightMethod"
+                    @highlight="setHighlightMethod"
+                />
+                <q-skeleton v-else type="text" class="c-trx-overview__skeleton" />
+            </q-td>
             <q-td key="date" :props="props">
                 <DateField :epoch="convertToEpoch(props.row.timestamp)" :force-show-age="showDateAge"/>
             </q-td>
@@ -313,7 +488,7 @@ onMounted(() => {
                     v-if="props.row.from"
                     :key="props.row.from"
                     :address="props.row.from"
-                    :truncate="16"
+                    :truncate="12"
                     :highlightAddress="highlightAddress"
                     @highlight="setHighlightAddress"
                 />
@@ -323,25 +498,27 @@ onMounted(() => {
                     v-if="props.row.to"
                     :key="props.row.to"
                     :address="props.row.to"
-                    :truncate="16"
+                    :truncate="12"
                     :highlightAddress="highlightAddress"
                     @highlight="setHighlightAddress"
                 />
             </q-td>
+            <q-td key="id" :props="props">
+                <span class="q-pl-xs" >
+                    #{{ truncatedId(props.row.id) }}
+                    <q-tooltip>{{ props.row.id }}</q-tooltip>
+                </span>
+            </q-td>
+            <q-td key="amount" :props="props">
+                <span>
+                    {{ truncatedId(props.row.amount) }}
+                    <q-tooltip>{{ props.row.amount }}</q-tooltip>
+                </span>
+            </q-td>
             <q-td key="value" :props="props">
-                <span v-if="tokenType==='erc721' && props.row.token?.imageCache">
-                    <q-img
-                        class="nft-icon"
-                        :src="props.row.token.imageCache + '/280.webp'"
-                    />
+                <span>
+                    {{ truncatedId(props.row.value) }}
                     <q-tooltip>{{ props.row.value }}</q-tooltip>
-                </span>
-                <span v-else-if="tokenType==='erc721' && props.row.value.length > 7">
-                    <span>{{ props.row.value.substr(0, 7) + "..." }}</span>
-                    <q-tooltip>{{ props.row.value }}</q-tooltip>
-                </span>
-                <span v-else>
-                    {{ props.row.value }}
                 </span>
             </q-td>
             <q-td key="token" :props="props" class="flex items-center">
@@ -352,10 +529,15 @@ onMounted(() => {
                     :highlightAddress="highlightAddress"
                     @highlight="setHighlightAddress"
                 />
-                <span v-if="tokenType === 'erc1155'" class="q-pl-xs" >
-                    #{{ truncatedId(props.row.id) }}
-                    <q-tooltip>{{ props.row.id }}</q-tooltip>
-                </span>
+            </q-td>
+            <q-td key="item" :props="props" class="flex items-center">
+                <AddressField
+                    :key="props.row.contract.address"
+                    :address="props.row.contract.address"
+                    :truncate="16"
+                    :highlightAddress="highlightAddress"
+                    @highlight="setHighlightAddress"
+                />
             </q-td>
         </q-tr>
     </template>
