cffcf32397d35ea3dad9515d79a1cb88ee006d41
Viterbo <viter.rod@gmail.com>
2024-07-30 15:22:06 -0300

    several style bugs fixed

--- src/components/Transaction/LogsViewer.vue
+++ src/components/Transaction/LogsViewer.vue
@@ -93,7 +93,7 @@ export default {
         </div>
     </div>
     <div v-else class="row">
-        <div class="col-12 u-flex--center-y justify-between">
+        <div class="col-12 u-flex--center-y justify-between q-pa-md">
             <div>
                 <q-toggle
                     v-model="human_readable"
