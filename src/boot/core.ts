6f8f5c8f3d691bd78fa0e05e6eade9475145b8e7
Viterbo <viter.rod@gmail.com>
2024-12-11 16:55:12 -0300

    big merge back to "redirect to network website" menu-link (but without rolling all back)

--- src/boot/core.ts
+++ src/boot/core.ts
@@ -6,7 +6,8 @@ export default boot(({ app }) => {
     initCore(app);
 
     const defaultNetwork = Object.keys(evmSettings)[0];
-    let network = new URLSearchParams(window.location.search).get('network');
+    let network = new URLSearchParams(window.location.search).get('network') ?? process.env.NETWORK_EVM_NAME;
+    console.log('Multichain initMultichain()', { network, NETWORK_EVM_NAME: process.env.NETWORK_EVM_NAME });
     if (network) {
         const exists = Object.keys(evmSettings).some(key => evmSettings[key].getNetwork() === network);
         if (!exists) {
